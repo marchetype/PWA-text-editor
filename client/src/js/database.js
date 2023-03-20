1421a44d9ef4925bd5bcf9e6fa6eb74e7f7fa54e
marchetype <114883447+marchetype@users.noreply.github.com>
2023-03-20 13:06:17 -0400

    work done on database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,10 +14,32 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  
+  console.log('PUT Request');
+  //connect to DB, select version in use, set up transaction, and open object store.
+  const contactDb = await openDB('jate', 1);
+  const transact = contactDb.transaction('jate', 'readwrite');
+  const store = transact.objectStore('jate');
+
+  const request = store.put({ id: 1, value: content });
+
+  const result = await request;
+  console.log('Data has been saved!', result);
+
 }
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET Request');
+  //connect to DB, select verion in use, set up transaction, and open object store.
+  const contactDb = await openDB('jate', 1);
+  const transact = contactDb.transaction('jate', 'readonly');
+  const store = transact.objectStore('jate');
+
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('result.value', result);
+  return result?.value;
+};
 
 initdb();
